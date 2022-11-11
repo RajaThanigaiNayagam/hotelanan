bae5720cb204b95e984a043400c369443dbf59c6
RajaThanigainayagam <thanigainayagam@yahoo.fr>
2022-11-12 00:40:25 +0100

    database test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include('includephp/connectionbd.php');
 $ret="SELECT * from tbladmin";
 $query1 = $dbh -> prepare($ret);
 $query1->execute();
-$resultss=$query1->fetchAll(PDO::FETCH_OBJ);
+$result=$query1->fetchAll(PDO::FETCH_OBJ);
 
 ?>
 <!DOCTYPE HTML>
@@ -15,7 +15,7 @@ $resultss=$query1->fetchAll(PDO::FETCH_OBJ);
 </head>
     <body>
         <div class="header">
-            Nom de l'Administrateur est  -  <?php echo htmlentities($resultss->AdminName)?>;
-        </div>">
+            Nom de l'Administrateur est  -  <?php echo htmlentities($result->AdminName)?>
+        </div>
     </body>
 </html>

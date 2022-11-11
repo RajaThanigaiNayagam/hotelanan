7493125d0f4e0f6509a0f331bba7f1cc7dc67692
RajaThanigainayagam <thanigainayagam@yahoo.fr>
2022-11-12 00:37:38 +0100

    database test to affiche le nom d'admin

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 //  apper le fichier  includephp/connectiondb.php
 include('includephp/connectionbd.php');
 
-$ret="SELECT * from tblcategory";
+$ret="SELECT * from tbladmin";
 $query1 = $dbh -> prepare($ret);
 $query1->execute();
 $resultss=$query1->fetchAll(PDO::FETCH_OBJ);
@@ -15,7 +15,7 @@ $resultss=$query1->fetchAll(PDO::FETCH_OBJ);
 </head>
     <body>
         <div class="header">
-            Nom de l'Administrateur est  -  <?php echo htmlentities($rows->ID)?>;
+            Nom de l'Administrateur est  -  <?php echo htmlentities($resultss->AdminName)?>;
         </div>">
     </body>
 </html>

9f6e8ea0cf83bf2533bef911922e6e63ffbaabe1
RajaThanigainayagam <thanigainayagam@yahoo.fr>
2022-11-12 00:02:59 +0100

    Added heroku + database + connection variables for a test

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-    ECHO "Hello World -- RAJA";
+    //Recup des info de Connection CLEARDB
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    
+    try
+    {
+        $dbh = new PDO("mysql:host=".$cleardb_server.";dbname=".$cleardb_db,$cleardb_username, $cleardb_password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    }
+    catch (PDOException $e)
+    {
+        exit("Error: " . $e->getMessage());
+    }
 ?>

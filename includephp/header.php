b437097d5ad3271b4bb953836947b401d4b3d3d5
RajaThanigainayagam <thanigainayagam@yahoo.fr>
2022-11-27 12:16:48 +0100

    Correction made for mobile phones

--- includephp/header.php
+++ includephp/header.php
@@ -11,7 +11,7 @@
         <!----------------------------------------------------------->
         <!-------------barre de navigation responsive---------------->
         <!----------------------------------------------------------->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ouvremenu" aria-controls="ouvremenu" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#ouvremenu">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-list" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
             </svg>
@@ -25,7 +25,7 @@
                     <a class="nav-link" href="about.php">A propos de</a>
                 </li>
                 <li class="nav-item">
-                    <li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Rooms</span></a>
+                    <li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Chambres</span></a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <?php
                                 $ret="SELECT * from roomcategory";
@@ -45,17 +45,17 @@
                 <?php if (strlen($_SESSION['hotelanan']==0)) 
                 {$_SESSION['hotelanan']?>
                     <li class="nav-item"><a class="nav-link" href="admin/login.php">Admin</a></li>
-                    <li class="nav-item"><a class="nav-link" href="signup.php">Sign Up</a></li>
-                    <li class="nav-item"><a class="nav-link" href="signin.php">Login</a></li>  <?php 
+                    <li class="nav-item"><a class="nav-link" href="signup.php">S'inscrire</a></li>
+                    <li class="nav-item"><a class="nav-link" href="signin.php">Connexion</a></li>  <?php 
                 } ?>
                 <?php if (strlen($_SESSION['hotelanan']!=0)) 
                 {?>
                     <!-- ----------Si l utilisateu est déja connectée---------- -->
                     <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown2" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account</a>
+                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown2" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mon compte</a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="profile.php">Profile</a></li>
-                            <li><a class="dropdown-item" href="my-booking.php">Mes reservation</a></li>
+                            <li><a class="dropdown-item" href="profile.php">Profil</a></li>
+                            <li><a class="dropdown-item" href="my-booking.php">Mes reservations</a></li>
                             <li><a class="dropdown-item" href="change-password.php">Changer le mot de passe</a></li>
                             <li><a class="dropdown-item" href="logout.php">Se déconnecter</a></li>
                         </ul>

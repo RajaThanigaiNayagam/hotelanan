b737765f4ea926e2e24c3523122ea6ea61088117
RajaThanigainayagam <thanigainayagam@yahoo.fr>
2023-03-04 17:33:21 +0100

    LOCALHOST database setting changed to HEORKU database setting

--- includephp/connectionbd.php
+++ includephp/connectionbd.php
@@ -1,13 +1,17 @@
 <?php 
-    // DB credentials.
-    define('DB_HOST','localhost');
-    define('DB_USER','root');
-    define('DB_PASS','');
-    define('DB_NAME','hotelanandhi');
-    // Establish database connection.
+    //Recup des info de Connection CLEARDB
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    
     try
     {
-        $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        $dbh = new PDO("mysql:host=".$cleardb_server.";dbname=".$cleardb_db,$cleardb_username, $cleardb_password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
     }
     catch (PDOException $e)
     {
